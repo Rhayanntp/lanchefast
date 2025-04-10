9995ffb1fdf56df4111d1be3caabb8edcfd233d2
Rhayanntp <rhayanrhangel58@gmail.com>
2025-04-10 12:35:20 -0300

    livewire feito index, seeder, model cliente, migration cliente, index cliente

---
+++ app/Livewire/Cliente/Index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire\Cliente;
+
+use App\Models\Cliente;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public $search = '';
+    public $perPage = 10;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'perPage' => ['except' => 10],
+    ];
+
+
+    public function render()
+    {
+        $clientes = Cliente::where('nome', 'like', "%{$this->search}%")
+        ->orWhere('email', 'like', "%{$this->search}%")
+        ->orWhere('cpf', 'like', "%{$this->search}%")
+        ->paginate($this->perPage);
+
+        return view('livewire.cliente.index', compact('clientes'));
+    }
+
+    public function delete($id)
+    {
+        Cliente::findOrFail($id)->delete();
+        session()->flash('message', 'Cliente deletado com sucesso.');
+    }
+}
---
+++ app/Livewire/Cliente/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Cliente;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.cliente.show');
+    }
+}
